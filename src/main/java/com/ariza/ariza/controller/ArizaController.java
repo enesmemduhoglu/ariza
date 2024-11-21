ca6bd3bc2511d9bd49aa31f11880d5fff8b8a760
Enes Memduhoglu <eneshan034@gmail.com>
2024-11-22 00:30:37 +0300

    addd

--- src/main/java/com/ariza/ariza/controller/ArizaController.java
+++ src/main/java/com/ariza/ariza/controller/ArizaController.java
@@ -1,17 +1,22 @@
 package com.ariza.ariza.controller;
 
 import com.ariza.ariza.model.Ariza;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ariza")
+@CrossOrigin
 public class ArizaController {
 
     @PostMapping
     public Ariza createAriza(@RequestBody Ariza ariza) {
         return ariza;
+    }
+
+    @GetMapping
+    public String handleGetRequest() {
+        return "GET method is not supported here";
     }
 }

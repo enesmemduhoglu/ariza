d92f196d4830643f0e83bba8c22c73d1fecbe13c
Enes Memduhoglu <eneshan034@gmail.com>
2024-11-21 14:08:29 +0300

    add2

--- src/main/java/com/ariza/ariza/controller/ArizaController.java
+++ src/main/java/com/ariza/ariza/controller/ArizaController.java
@@ -1,8 +1,6 @@
 package com.ariza.ariza.controller;
 
-import com.ariza.ariza.Repository.ArizaRepository;
 import com.ariza.ariza.model.Ariza;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ariza")
 public class ArizaController {
 
-    @Autowired
-    private ArizaRepository arizaRepository;
-
     @PostMapping
     public Ariza createAriza(@RequestBody Ariza ariza) {
-        return arizaRepository.save(ariza);
+        return ariza;
     }
 }
